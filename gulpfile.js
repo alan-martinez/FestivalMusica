5249e5ca68e8e36645f9956e56619b0f3bf4c0b6
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-10 14:18:48 -0500

    header y colores

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
-function tarea(done){
-    console.log("Mi primera tarea");
+function css(done){
+
+    src('src/SCSS/**/*.scss')//Identificar el archivo SASS
+        .pipe(plumber())//Para que no se detenga el proceso si hay un error
+        .pipe(sass())//Compilarlo el archivo
+        .pipe(dest("build/css"));//Almacenar en disco duro
+
     done();
 }
 
-exports.tarea = tarea;
+function dev(done){
+    watch('src/SCSS/**/*.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
